1e3ffb17ccd6d50e4888f6dc356ddb719c0552ad
RIDHISHA HEDE <ridhishahede2903@gmail.com>
2024-05-16 00:21:07 +0530

    Convert to Upper Case enabled

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,12 +4,23 @@ export default function TextForm(props) {
 
     const [text, setText] = useState("Enter Text here");
 
+    const handleUpperCase = ()=> {
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+    const handleChange = (event)=> {
+        console.log("User is trying to update the text");
+        setText(event.target.value);
+    }
+
   return (
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} id="exampleFormControlTextarea1" rows="10"></textarea>
+            <textarea className="form-control" value={text} onChange={handleChange} id="exampleFormControlTextarea1" rows="10"></textarea>
         </div>
+        <button type="button" class="btn btn-primary" onClick={handleUpperCase}>Convert to UpperCase</button>
     </div>
   )
 }
