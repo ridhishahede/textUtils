afe453475df3cf3985fa7a7b367a9008b45b9661
RIDHISHA HEDE <ridhishahede2903@gmail.com>
2024-05-16 00:50:08 +0530

    added more functionalities

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,11 +2,16 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
 
-    const [text, setText] = useState("Enter Text here");
+    const [text, setText] = useState('');
 
     const handleUpperCase = ()=> {
         let newText = text.toUpperCase();
-        setText(newText)
+        setText(newText);
+    }
+
+    const handleLowerCase = ()=> {
+        let lowerText = text.toLowerCase();
+        setText(lowerText);
     }
 
     const handleChange = (event)=> {
@@ -15,12 +20,24 @@ export default function TextForm(props) {
     }
 
   return (
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleChange} id="exampleFormControlTextarea1" rows="10"></textarea>
+    <>
+        <div className='container'>
+            <h1>{props.heading}</h1>
+            <div className="container mb-3">
+                <textarea className="form-control" value={text} onChange={handleChange} id="exampleFormControlTextarea1" rows="10"></textarea>
+                <button type="button" class="btn btn-primary my-3 mx-2" onClick={handleUpperCase}>Convert to UpperCase</button>
+                <button type="button" class="btn btn-primary my-3 mx-2" onClick={handleLowerCase}>Convert to LowerCase</button>
+            </div>
         </div>
-        <button type="button" class="btn btn-primary" onClick={handleUpperCase}>Convert to UpperCase</button>
-    </div>
+        <div className="conatiner my-5">
+            <h3>Your text report</h3>
+            <p>{text.split(" ").length} words, {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} minutes read</p>
+        </div>
+        <div className="container">
+            <h3>Preveiw</h3>
+            <p>{text}</p>
+        </div>
+    </>
   )
 }
