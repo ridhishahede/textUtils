a30336aabf67c7cb0232e9c18140c034cdaf1d11
RIDHISHA HEDE <ridhishahede2903@gmail.com>
2024-05-16 01:24:44 +0530

    added extra functionalities

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,6 +3,13 @@ import React, { useState } from 'react'
 export default function TextForm(props) {
 
     const [text, setText] = useState('');
+    const [syllables, setSyllables] = useState(0);
+
+    const countSyllables = () => {
+      const regex = /[aeiouAEIOU]+/g;
+      const matches = text.match(regex);
+      setSyllables(matches.length);
+    };
 
     const handleUpperCase = ()=> {
         let newText = text.toUpperCase();
@@ -12,6 +19,17 @@ export default function TextForm(props) {
     const handleLowerCase = ()=> {
         let lowerText = text.toLowerCase();
         setText(lowerText);
+    }
+
+    const handleCopy = ()=> {
+        navigator.clipboard.writeText(text);
+        alert("Text copied to Clipboard!");
+    }
+
+    const handleClearText = ()=> {
+        let clearText = "";
+        setSyllables(0);
+        setText(clearText);
     }
 
     const handleChange = (event)=> {
@@ -27,12 +45,16 @@ export default function TextForm(props) {
                 <textarea className="form-control" value={text} onChange={handleChange} id="exampleFormControlTextarea1" rows="10"></textarea>
                 <button type="button" class="btn btn-primary my-3 mx-2" onClick={handleUpperCase}>Convert to UpperCase</button>
                 <button type="button" class="btn btn-primary my-3 mx-2" onClick={handleLowerCase}>Convert to LowerCase</button>
+                <button type="button" class="btn btn-primary my-3 mx-2" onClick={handleCopy}>Copy to ClipBoard</button>
+                <button type="button" class="btn btn-primary my-3 mx-2" onClick={countSyllables}>Count Syllables</button>
+                <button type="button" class="btn btn-primary my-3 mx-2" onClick={handleClearText}>Clear Text</button>
             </div>
         </div>
         <div className="conatiner my-5">
             <h3>Your text report</h3>
             <p>{text.split(" ").length} words, {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} minutes read</p>
+            <p>Syllables: {syllables}</p>
         </div>
         <div className="container">
             <h3>Preveiw</h3>
